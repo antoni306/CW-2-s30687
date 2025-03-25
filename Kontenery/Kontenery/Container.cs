f3484cf99c90598d466d172bc99269debda3b1e3
Antoni <s30687@pjwstk.edu.pl>
2025-03-25 15:18:11 +0100

    Created Container class and OverfillException

---
+++ Kontenery/Kontenery/Container.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontenery
+{
+    abstract class Container
+    {
+        private static List<int> _containersNumbers = new List<int>();
+
+        public double PropertyWeight { get; set; }
+        public double Height { get; }
+        public double ContainerWeight { get; }
+        public double Depth { get; }
+        public string SerialNumber { get; }
+        public double MaxCapacity { get; }
+        public Container(double propWeight,double height,double contWeight,double depth,double maxCapacity)
+        {
+            PropertyWeight = propWeight;
+            Height = height;
+            ContainerWeight = contWeight;
+            Depth = depth;
+            MaxCapacity = maxCapacity;
+        }
+        protected abstract void generateSerialNumber();
+
+        private int generateNumber()
+        {
+            Random rand = new Random();
+            int number= rand.Next(0,1000);
+            for (int i = 0; i < _containersNumbers.Count(); i++)
+            {
+                if (number == _containersNumbers[i])
+                    generateNumber();
+            }
+            _containersNumbers.Add(number);
+            return number;
+        }
+        public void loadOut()
+        {
+            PropertyWeight = 0;
+        }
+        public void loadIn(double weight)
+        {
+            if (weight + PropertyWeight > MaxCapacity)
+                throw new OverfillException();
+        }
+        
+
+
+
+    }
+    
+}
---
+++ Kontenery/Kontenery/OverfillException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Kontenery
+{
+    [Serializable]
+    internal class OverfillException : Exception
+    {
+        public OverfillException()
+        {
+        }
+
+        public OverfillException(string message) : base(message)
+        {
+        }
+
+        public OverfillException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected OverfillException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
